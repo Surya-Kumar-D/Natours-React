13e69a0076ac317cf1108bbcf0c5b2ec6a34b0cb
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-09-04 13:29:59 +0530

    learned how css works behind the scenes

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ function App() {
   return (
     <div>
       <header className="header">
-        <div className="logo-box">
-          <img className="logo" src="logo-white.png" alt="Logo" />
+        <div className="header__logo-box">
+          <img className="header__logo" src="logo-white.png" alt="Logo" />
         </div>
-        <h1 className="heading-primary">
-          <span className="heading-primary-main">Outdoors</span>
-          <span className="heading-primary-sub">is where life happens</span>
-        </h1>
+        <div className="header__text-box">
+          <h1 className="heading-primary">
+            <span className="heading-primary--main">Outdoors</span>
+            <span className="heading-primary--sub">is where life happens</span>
+          </h1>
+          <a href="#" className="btn btn--white btn--animated">
+            Discover our tours
+          </a>
+        </div>
       </header>
     </div>
   );
