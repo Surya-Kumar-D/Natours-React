157fd2fcd0da916ffa839f7e251fde8c38524384
Surya-Kumar-D <suryakumar.dhanasekar@gmail.com>
2024-09-05 13:35:55 +0530

    changed the architecture to 7-1 css architecture

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./styles/main.scss";
-// import "./style.css";
-import App from "./components/App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './sass/main.scss';
+// import './style.css';
+import App from './components/App';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
